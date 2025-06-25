494b868b6c33523a6060c9ad7eb32b4cd2776ca5
onurdincel <s.onurdincel@gmail.com>
2025-06-25 11:30:03 +0300

    added sys argv

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -8,7 +9,12 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key = api_key)
 
 model = "gemini-2.0-flash-001"
-content = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+
+if len(sys.argv) != 2:
+    print("need one argument")
+    sys.exit(1)
+
+content = sys.argv[1]
 
 response = client.models.generate_content(
         model = model, contents = content
