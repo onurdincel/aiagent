16a25ab4b727c876b7f8fee7f447bc4d73bb33ae
onurdincel <s.onurdincel@gmail.com>
2025-06-26 13:30:44 +0300

    change input and added --verbose

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+
 import sys
 
 load_dotenv()
@@ -10,20 +12,26 @@ client = genai.Client(api_key = api_key)
 
 model = "gemini-2.0-flash-001"
 
-if len(sys.argv) != 2:
+if len(sys.argv) < 1:
     print("need one argument")
     sys.exit(1)
 
-content = sys.argv[1]
+user_prompt = sys.argv[1]
+
+messages = [
+        types.Content(role = "user", parts=[types.Part(text=user_prompt)]),
+    ]
 
 response = client.models.generate_content(
-        model = model, contents = content
+        model = model, contents = messages
         )
 
 print(response.text)
 
-promt_token_count = response.usage_metadata.prompt_token_count
-candidate_token_count = response.usage_metadata.candidates_token_count
-
-print(f"Prompt tokens: {promt_token_count}")
-print(f"Response tokens: {candidate_token_count}")
+if "--verbose" in sys.argv:
+    promt_token_count = response.usage_metadata.prompt_token_count
+    candidate_token_count = response.usage_metadata.candidates_token_count
+    
+    print(f"User prompt: {user_prompt}")
+    print(f"Prompt tokens: {promt_token_count}")
+    print(f"Response tokens: {candidate_token_count}")
